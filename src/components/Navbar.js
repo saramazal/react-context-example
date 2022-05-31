fb19f775089eba0f36e8fc26d4530b8ec1f6a305
Sara Mazal <mazalsara@gmail.com>
2022-05-31 05:50:59 +0000

    reading list

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
   const { books } = useContext(BookContext);
   return (
     <header className="navbar">
-      <h1>My Reading List</h1>
+      <h1>Reading List</h1>
       <h2>Currently you have {books.length} books to get through...</h2>
     </header>
   );
