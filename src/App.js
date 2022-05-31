88a558e1ce0291569a4246d299ee022ecd1a21ed
Sara Mazal <mazalsara@gmail.com>
2022-05-31 06:23:09 +0000

    footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import BookContextProvider from './contexts/BookContext';
 import BookList from './components/BookList';
 import NewBookForm from './components/NewBookForm';
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
          <NewBookForm />
          <BookList />
+         <Footer />
       </BookContextProvider>
     </div>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer>
- <p className="footer-text">Sara Mazal</p>
+    <footer> 
+ <a className="footer-text" href="https://github.com/saramazal/react-context-example">Github repo @saramazal</a>
     
     </footer>
   )
